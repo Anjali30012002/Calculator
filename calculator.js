9ad6f556a5c509ddcf658b6d8e054dcf9858b499
Anjali30012001 <anjali2000130@gmail.com>
2024-03-14 15:54:23 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,79 @@
+
+
+var screen = document.querySelector('#screen');
+var btn = document.querySelectorAll('.btn');
+
+// For getting the value of btn, Here i am using for loop ...
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        btntext = e.target.innerText;
+
+        if (btntext == '×') {
+            btntext = '*';
+        }
+
+        if (btntext == '÷') {
+            btntext = '/';
+        }
+        screen.value += btntext;
+    });
+}
+
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+    const val = 2;
+    screen.value = Math.pow(screen.value, val);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+    screen.value = Math.log(screen.value);
+}
+
+function pi() {
+    screen.value = 3.14159265359;
+}
+
+function e() {
+    screen.value = 2.71828182846;
+}
+
+function fact() {
+    var i, num, f;
+    f = 1
+    num = screen.value;
+    for (i = 1; i <= num; i++) {
+        f = f * i;
+    }
+
+    i = i - 1;
+
+    screen.value = f;
+}
+
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
+}
+function calculate(){
+    var splitscreen=screen.value.split('%',2);
+    if(splitscreen.length>1){
+        screen.value=eval(splitscreen[0]+'/100*'+splitscreen[1]);
+    }
+    else{
+        screen.value=eval(screen.value);
+    }
+}
